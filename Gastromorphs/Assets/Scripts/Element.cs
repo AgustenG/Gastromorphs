b9799295a4d8dfb3170be8c148549a3f4ef86a3a
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-02-09 14:42:22 +0100

    update clases

--- Gastromorphs/Assets/Scripts/Element.cs
+++ Gastromorphs/Assets/Scripts/Element.cs
@@ -1,6 +1,6 @@
 public class Element : Attribute
 {
-    public Element(int id, string name, string description, string IconURI)
+    public Element(int id, string name, string description, string IconURI) //: base(id, name, description, IconURI)
     {
         this.id = id;
         this.name = name;
--- Gastromorphs/Assets/Scripts/Gastromorph.cs
+++ Gastromorphs/Assets/Scripts/Gastromorph.cs
@@ -1,15 +1,19 @@
 using System.Collections.Generic;
 
-public class Gastromorph : Attribute
+public class Gastromorph 
 {
+    private int gastromorph_id;
+    private string name;
+    private string description;
     private List<Biome> biomes;
     private List<Element> elements;
     private List<Flavour> flavours;
+    private string IconURI;
     private List<string> AnimURI;
 
-    public Gastromorph(int id, string name, string description, List<Biome> biomes, List<Element> elements, List<Flavour> flavours, string IconURI, List<string> AnimURI)
+    public Gastromorph(int gastromorph_id , string name, string description, List<Biome> biomes, List<Element> elements, List<Flavour> flavours, string IconURI, List<string> AnimURI)
     {
-        this.id = id;
+        this.gastromorph_id = gastromorph_id;
         this.name = name;
         this.description = description;
         this.biomes = biomes;
