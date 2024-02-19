33ad3ffaa0348300ac1cc9614014e492d6f61a4a
AgusITB <agustin.garibotto.7e6@itb.cat>
2024-02-19 16:06:12 +0100

    gastromorph select

--- Gastromorphs/Assets/Scripts/SqliteExampleSimple.cs
+++ Gastromorphs/Assets/Scripts/SqliteExampleSimple.cs
@@ -1,30 +1,52 @@
 using UnityEngine;
 using Mono.Data.Sqlite;
 using System.Data;
-using System.Linq;
 
 public class SqliteExampleSimple : MonoBehaviour
 {
-    public Texture2D TexRef;
     IDbConnection dbConnection;
     IDbCommand dbCommandInsertValue;
+    IDbCommand dbCommandReadValue;
+    public enum Tables
+    {
+        Biome,
+        Element,
+        Flavour
+    }
 
-    string[] insertableTables = { "Biome", "Element", "Flavour" };
     private void Awake()
     {
         dbConnection = CreateAndOpenDatabase();
         dbCommandInsertValue = dbConnection.CreateCommand();
+        dbCommandReadValue = dbConnection.CreateCommand();
+
     }
 
     private void OnDisable()
     {
         dbConnection.Close();
-        Debug.Log("Database disconnection");
+        Debug.Log("Database disconnected");
     }
     public void Click()
     {
-        DataBaseAction(InsertGastromorph("Guindilava", "Guindilla de lava", 1, 1, 1, "test.png", "test.fbx"));
+        GetGastromorphs();
+
     }
+
+    private void GetGastromorphs()
+    {
+        dbCommandReadValue.CommandText = "SELECT * FROM \"main\".\"Gastromorphs\"";
+        IDataReader dataReader = dbCommandReadValue.ExecuteReader();
+        
+        while (dataReader.Read()) 
+        {
+            Gastromorph gastromorph = new Gastromorph(dataReader.GetInt32(0), dataReader.GetString(1));
+            Debug.Log(gastromorph.Name + ": " + gastromorph.Gastromorph_id);
+        }
+        dbCommandReadValue.Parameters.Clear();  
+        dataReader.Close();
+    }
+
     private void DataBaseAction(string action)
     {
         try
@@ -37,19 +59,20 @@ public class SqliteExampleSimple : MonoBehaviour
             Debug.Log("no");
             throw;
         }
-        
+
+    }
+    private string SelectAllGastromorphs()
+    {
+        return "SELECT * FROM \"main\".\"Gastromorphs\"";
     }
     private string DeleteTableData(string databasename)
     {
         return "DELETE FROM \"main\".\"Gastromorphs\"";
     }
-    private string InsertAttributeIntoTable(string table, string name, string desc, string iconuri)
+    private string InsertAttributeIntoTable(Tables tab, string name, string desc, string iconuri)
     {
-        if (insertableTables.Contains<string>(table)) {
-
-            return $"INSERT INTO \"main\".\"{table}\"\r\n(\"name\", \"description\", \"IconUri\")\r\nVALUES ('{name}','{desc}','{iconuri}')";
-        }
-        return null;
+        string table = tab.ToString();
+        return $"INSERT INTO \"main\".\"{table}\"\r\n(\"name\", \"description\", \"IconUri\")\r\nVALUES ('{name}','{desc}','{iconuri}')";
     }
     private string InsertGastromorph(string name, string desc, int biome_id, int element_id, int flavour_id, string iconuri, string modelUri)
     {
