80f8df098148492c595b78e1d4247befd1c2ed79
OlavMartos <olav.martos.7e4@itb.cat>
2024-03-03 12:52:14 +0100

    Splash Screen Background Fixed

--- Gastromorphs/Assets/Scripts/SplashScreen.cs
+++ Gastromorphs/Assets/Scripts/SplashScreen.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SplashScreen : MonoBehaviour
 {
     public float waitTime;
+    public VideoPlayer video;
     void Start()
     {
         StartCoroutine(WaitForIntro());
     }
 
-    // Update is called once per frame
     IEnumerator WaitForIntro()
     {
+        video.Play();
         yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene(1);
     }
